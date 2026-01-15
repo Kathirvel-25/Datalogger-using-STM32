60ceb2cdf7b67cf8c614ddcbaecaa21b31b5bb6e
Kathirvel A <lmbmkathirvela@gmail.com>
2026-01-15 10:10:56 +0530

    Update W25Q_Driver.h

--- inc/W25Q_Driver.h
+++ inc/W25Q_Driver.h
@@ -12,5 +12,7 @@ void W25_Pageprogram(uint32_t addr,uint8_t *rdata,uint16_t len);
 void Sector_Erase(uint32_t addr);
 void W25_SectorErase(uint32_t address);
 void Is_Bussy(void);
+void Read_Id(uint8_t *id);
 
 #endif /* W25Q_DRIVER_H_ */
+
